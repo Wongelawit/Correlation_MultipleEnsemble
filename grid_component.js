7d415652b6648ec2b35383c6ccbb99d78b1918e4
Caitlin Coyiuto <t2b1b@ugrad.cs.ubc.ca>
2018-10-27 01:01:52 -0700

    Grid component plugin.

---
+++ grid_component.js
@@ -0,0 +1,25 @@
+/**
+  * Generates jsPsych trial object using multiple-ensembles-grid object.
+  * Creates a grid of specified dimension and will display one target
+  * among a distractor population. 
+  *
+  * @param  distractor           {string} Path to img of distractor
+  *			target				 {string} Path to img of target
+  *			duration 			 {int}    Time in ms for grid to display
+  *         grid_dimension       {int}	  Dimension of the grid
+  *			distractor_size		 {int}    Size of the distractor population
+  *
+  * @return [row, col] (JSON stringified so can do object comparisons)        
+*/ 
+function generate_grid_object (distractor, target, duration, grid_dimension, distractor_size){
+
+	let stimuli_set = [distractor, target];
+
+	return {
+	    type: 'multiple-ensembles-grid',
+        trial_duration: duration,
+        grid_size: [grid_dimension, grid_dimension],
+        distractor_number: distractor_size,
+	    stimuli: stimuli_set
+	};
+}
