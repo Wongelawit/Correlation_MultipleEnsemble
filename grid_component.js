ac293640014ccf50eaddefc9ddb4160914eea296
Wongelawit Teka Zewde <gospel.teka@gmail.com>
2018-10-30 09:36:17 -0700

    #

---
+++ grid_component.js
@@ -0,0 +1,24 @@
+/**
+ * Generates jsPsych trial object using multiple-ensembles-grid object.
+ * Creates a grid of specified dimension and will display target population
+ * among a distractor population.
+ *
+ * @param  distractor_path           {string} Path to img of distractor
+ *         target_path               {string} Path to img of target
+ *         duration                  {int}    Time in ms for grid to display
+ *         grid_dimension            {int}    Dimension of the grid
+ *         distractor_size           {int}    Size of the distractor population
+ *         target_size               {int}    Size of the target population
+ *
+ * @return jsPsych trial object
+ */
+function generate_grid_object (distractor_path, target_path, duration, grid_dimension, distractor_size, target_size){
+
+    return {
+        type: 'multiple-ensembles-grid',
+        trial_duration: duration,
+        grid_size: [grid_dimension, grid_dimension],
+        distribution_sizes: [distractor_size, target_size],
+        stimuli: [distractor_path, target_path]
+    };
+}
--- jspsych-6.0.5/plugins/jspsych-multiple-ensembles-grid.js
+++ jspsych-6.0.5/plugins/jspsych-multiple-ensembles-grid.js
@@ -1,5 +1,5 @@
 /**
- * jsPsych plugin for generating a grid of specified size, stimuli, and 
+ * jsPsych plugin for generating a grid of specified size, stimuli, and
  * distractor number.
  *
  *
@@ -8,186 +8,168 @@
 
 jsPsych.plugins['multiple-ensembles-grid'] = (function() {
 
-  var plugin = {};
+    var plugin = {};
 
-  jsPsych.pluginAPI.registerPreload('multiple-ensembles-grid', 'stimuli', 'image');
+    jsPsych.pluginAPI.registerPreload('multiple-ensembles-grid', 'stimuli', 'image');
 
-  plugin.info = {
-    name: 'multiple-ensembles-grid',
-    description: '',
-    parameters: {
-      stimuli: {
-        type: jsPsych.plugins.parameterType.IMAGE,
-        pretty_name: 'Stimuli',
-        array: true,
-        default: undefined,
-        description: 'An array that defines the stimuli.'
-      },
-      grid_size: {
-        type: jsPsych.plugins.parameterType.INT,
-        pretty_name: 'Grid size',
-        array: true,
-        default: [10,10],
-        description: 'Array specifying the row and columns of the grid.'
-      },
-      distractor_number: {
-        type: jsPsych.plugins.parameterType.INT,
-        pretty_name: 'Distractor number',
-        array: true,
-        default: 10,
-        description: 'Number specifying number of distractors in grid.'
-      },
-      trial_duration: {
-        type: jsPsych.plugins.parameterType.INT,
-        pretty_name: 'Trial duration',
-        default: 1000,
-        description: 'How long to show the stimulus for in milliseconds.'
-      },
-	  target_number: {
-        type: jsPsych.plugins.parameterType.INT,
-        pretty_name: 'Target number',
-        array: true,
-        default: 10,
-        description: 'Number specifying number of targets in grid.'
-      }
-    }
-  }
-
-  plugin.trial = function(display_element, trial) {
-
-    display_element.innerHTML = plugin.generate_stimulus(trial.stimuli, trial.grid_size, trial.distractor_number, trial.target_number);
-
-    jsPsych.pluginAPI.setTimeout(function() {
-      //endTrial();                               // !!! Uncomment this to enable trial_duration
-    }, trial.trial_duration);
-
-    function endTrial() {
-
-      display_element.innerHTML = '';
-
-      var trial_data = {
-        "stimulus": JSON.stringify(trial.stimuli)
-      };
-
-      jsPsych.finishTrial(trial_data);
-    }
-  };
-
-
-  plugin.generate_stimulus = function(stimuli, grid_size, distractor_number, target_number) {
-
-    let rows = grid_size[0];
-    let columns = grid_size[1];
-
-    let distractor = stimuli[0];
-    let target = stimuli[1];
-
-    let item_height = window.innerWidth/100;
-    let item_width = window.innerHeight/100;
-
-    let distractor_html = '<div class="grid-item" style="height: 100%; width: 100%""><img src = "' + distractor + '"</img></div>';
-    let target_html     = '<div class="grid-item" style="height: 100%; width: 100%""><img src = "' + target + '"</img></div>';
-    let empty_item_html = `<div class="grid-item" style="height: ${item_height}px; width: ${item_width}px""></div>`;
-
-    let html = 
-    `<div class='grid-container' style = 'grid-template-columns: repeat(${columns}, minMax(10px, 1fr));` +
-    ` grid-template-rows: repeat("${rows}"}, minMax(10px, 1fr));'>`;
-
-    let target_coordinates = generate_target_coordinates(rows, columns, target_number);
-    let distractor_coordinates = generate_coordinates(rows, columns, distractor_number, target_coordinates);
-
-    for (let r = 0; r < rows; r++){
-      for (let c = 0; c < columns; c++){
-
-        let curr_coord = JSON.stringify([r, c]);
-
-        if (distractor_coordinates.includes(curr_coord)) {
-          html += distractor_html;
-        } else if (target_coordinates.includes(curr_coord)) {
-          html += target_html;
-        } else {
-          html += empty_item_html;
+    plugin.info = {
+        name: 'multiple-ensembles-grid',
+        description: '',
+        parameters: {
+            stimuli: {
+                type: jsPsych.plugins.parameterType.IMAGE,
+                pretty_name: 'Stimuli',
+                array: true,
+                default: undefined,
+                description: 'An array that defines the stimuli.'
+            },
+            grid_size: {
+                type: jsPsych.plugins.parameterType.INT,
+                pretty_name: 'Grid size',
+                array: true,
+                default: [10,10],
+                description: 'Array specifying the row and columns of the grid.'
+            },
+            distribution_sizes: {
+                type: jsPsych.plugins.parameterType.INT,
+                pretty_name: 'Distractor number',
+                array: true,
+                default: [10, 1],
+                description: 'Array specifying target and distractor distribution sizes.'
+            },
+            trial_duration: {
+                type: jsPsych.plugins.parameterType.INT,
+                pretty_name: 'Trial duration',
+                default: 1000,
+                description: 'How long to show the stimulus for in milliseconds.'
+            }
         }
-      }
     }
 
-    return html;
+    plugin.trial = function(display_element, trial) {
 
-  };
+        display_element.innerHTML = plugin.generate_stimulus(trial.stimuli, trial.grid_size, trial.distribution_sizes);
 
-  /**
-   * Generates random coordinate given row and column values.
-   *
-   * @param  row                  {int}
-   *         col                  {int}
-   *
-   * @return [row, col] (JSON stringified so can do object comparisons)        
-   */ 
-  function generate_random_coordinate(row, col) {
+        jsPsych.pluginAPI.setTimeout(function() {
+            //endTrial();                               // !!! Uncomment this to enable trial_duration
+        }, trial.trial_duration);
 
-    let x = get_random_int(row);
-    let y = get_random_int(col);
+        function endTrial() {
 
-    return JSON.stringify([x, y]);
-  }
+            display_element.innerHTML = '';
+
+            var trial_data = {
+                "stimulus": JSON.stringify(trial.stimuli)
+            };
+
+            jsPsych.finishTrial(trial_data);
+        }
+    };
+
+
+    plugin.generate_stimulus = function(stimuli, grid_size, distribution_size) {
+
+        let rows = grid_size[0];
+        let columns = grid_size[1];
+
+        let distractor = stimuli[0];
+        let target = stimuli[1];
+
+        let distractor_number = distribution_size[0];
+        let target_number = distribution_size[1];
+
+        // Extract img dimensions so can force empty boxes to be of same height/width
+        let img = new Image();
+        img.src = distractor;
+        let item_width = img.width;
+        let item_height = img.height;
+
+        let distractor_html = '<div class="grid-item"><img src = "' + distractor + '"</img></div>';
+        let target_html     = '<div class="grid-item""><img src = "' + target + '"</img></div>';
+        let empty_item_html = `<div class="grid-item" style="height: ${item_height}px; width: ${item_width}px""></div>`;
+
+        let html =
+            `<div class='grid-container' style = 'grid-template-columns: repeat(${columns}, minMax(10px, 1fr));` +
+            ` grid-template-rows: repeat("${rows}"}, minMax(10px, 1fr));'>`;
+
+        let target_coordinates = generate_coordinates(rows, columns, target_number, null);
+        let distractor_coordinates = generate_coordinates(rows, columns, distractor_number, target_coordinates);
+
+        for (let r = 0; r < rows; r++){
+            for (let c = 0; c < columns; c++){
+
+                let curr_coord = JSON.stringify([r, c]);
+
+                if (distractor_coordinates.includes(curr_coord)) {
+                    html += distractor_html;
+                } else if (target_coordinates.includes(curr_coord)) {
+                    html += target_html;
+                } else {
+                    html += empty_item_html;
+                }
+
+            }
+        }
+
+        return html;
+
+    };
 
     /**
-   * Generates a random population.
-   *
-   * @param  row                  {int}
-   *         col                  {int}
-   *         size                 {int}
-   *
-   * @return coordinates          [ [x1, y1], [x2, y2] ... ]         
-   */ 
-  function generate_target_coordinates(row, col, size) {
+     * Generates random coordinate given row and column values.
+     *
+     * @param  row                  {int}
+     *         col                  {int}
+     *
+     * @return [row, col] (JSON stringified so can do object comparisons)
+     */
+    function generate_random_coordinate(row, col) {
 
-    let coordinates = [];
+        let x = get_random_int(row);
+        let y = get_random_int(col);
 
-    while (coordinates.length < size) {
-      let coord = generate_random_coordinate(row, col);
-
-      if (!coordinates.includes(coord)){
-        coordinates.push(coord);
-      }
-    }
-    return coordinates;
-  }
-  
-  /**
-   * Generates a random population that excludes a coordinate.
-   *
-   * @param  row                  {int}
-   *         col                  {int}
-   *         size                 {int}
-   *         excluding_coordinate [row, col]
-   *
-   * @return coordinates          [ [x1, y1], [x2, y2] ... ]         
-   */ 
-  function generate_coordinates(row, col, size, excluding_coordinate) {
-
-    let coordinates = [];
-
-    while (coordinates.length < size) {
-      let coord = generate_random_coordinate(row, col);
-
-      if (!coordinates.includes(coord) && !excluding_coordinate.includes(coord)){
-        coordinates.push(coord);
-      }
+        return JSON.stringify([x, y]);
     }
 
-    return coordinates;
-  }
+    /**
+     * Generates a random population, that can exclude a coordinate set if desired.
+     *
+     * @param  row                   {int}
+     *         col                   {int}
+     *         size                  {int}
+     *         excluding_coordinates {array of coordinates to exclude}
+     *                               Set this to null if do not want to exclude anything
+     *
+     * @return coordinates          [ [x1, y1], [x2, y2] ... ]
+     */
+    function generate_coordinates(row, col, size, excluding_coordinates) {
 
-  /**
-   * Generates random integer given max value.
-   *
-   * @param  max                   {int}
-   * @return integer     
-   */ 
-  function get_random_int(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-  }
+        let coordinates = [];
 
-  return plugin;
+        while (coordinates.length < size) {
+            let coord = generate_random_coordinate(row, col);
+
+            if (!coordinates.includes(coord)){
+                if (excluding_coordinates === null){
+                    coordinates.push(coord);
+                } else if (!excluding_coordinates.includes(coord)) {
+                    coordinates.push(coord);
+                }
+            }
+        }
+
+        return coordinates;
+    }
+
+    /**
+     * Generates random integer given max value.
+     *
+     * @param  max                   {int}
+     * @return integer
+     */
+    function get_random_int(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    return plugin;
 })();
