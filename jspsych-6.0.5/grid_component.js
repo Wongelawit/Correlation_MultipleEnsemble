bd88b8a7d0f912607f64e2f081918dc86ff21f97
Wongelawit Teka Zewde <gospel.teka@gmail.com>
2018-10-30 08:48:51 -0700

    #

--- jspsych-6.0.5/grid_component.js
+++ jspsych-6.0.5/grid_component.js
@@ -1,7 +1,7 @@
 /**
   * Generates jsPsych trial object using multiple-ensembles-grid object.
   * Creates a grid of specified dimension and will display one target
-  * among a distractor population.
+  * among a distractor population. 
   *
   * @param  distractor           {string} Path to img of distractor
   *     target                   {string} Path to img of target
@@ -9,14 +9,14 @@
   *     grid_dimension           {int}    Dimension of the grid
   *     distractor_size          {int}    Size of the distractor population
   *
-  * @return jsPsych trial object
-*/
+  * @return jsPsych trial object      
+*/ 
 function generate_grid_object (distractor, target, duration, grid_dimension, distractor_size, target_size){
 
   let stimuli_set = [distractor, target];
 
   return {
-        type: 'multiple-ensembles-grid',
+      type: 'multiple-ensembles-grid',
         trial_duration: duration,
         grid_size: [grid_dimension, grid_dimension],
         distractor_number: distractor_size,
