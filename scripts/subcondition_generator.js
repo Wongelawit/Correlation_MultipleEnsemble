65b02ae6d86c015f854ba88a6254801f7fc43f9b
fordat <fordatwater@gmail.com>
2018-11-06 20:38:56 -0800

    added hex codes to data

--- scripts/subcondition_generator.js
+++ scripts/subcondition_generator.js
@@ -7,80 +7,80 @@ const SET_SIZES = [8, 16, 32];
 
 const HEX_COLORS =
 // BLUES
-{"BLUE_LUM-2" : "#005a95",
-"BLUE_LUM-1" : "#006ca9",
+{"BLUE_LUM_-2" : "#005a95",
+"BLUE_LUM_-1" : "#006ca9",
 "BLUE_LUM" : "#007ebc", // TARGET
-"BLUE_LUM+1" : "#2391d1",
-"BLUE_LUM+2" : "#43a4e6",
+"BLUE_LUM_+1" : "#2391d1",
+"BLUE_LUM_+2" : "#43a4e6",
 
-"BLUE_CHR-2" : "#6a7887e",
-"BLUE_CHR-1" : "#4d7ba1",
+"BLUE_CHR_-2" : "#6a7887e",
+"BLUE_CHR_-1" : "#4d7ba1",
 "BLUE_CHR" : "#007ebc", // TARGET
-"BLUE_CHR+1" : "#0081d9",
-"BLUE_CHR+2" : "#0085f7",
+"BLUE_CHR_+1" : "#0081d9",
+"BLUE_CHR_+2" : "#0085f7",
 
-"BLUE_HUE-2" : "#008897",
-"BLUE_HUE-1" : "#0085ad",
+"BLUE_HUE_-2" : "#008897",
+"BLUE_HUE_-1" : "#0085ad",
 "BLUE_HUE" : "#007ebc", // TARGET
-"BLUE_HUE+1" : "#5575bc",
-"BLUE_HUE+2" : "#816ab1",
+"BLUE_HUE_+1" : "#5575bc",
+"BLUE_HUE_+2" : "#816ab1",
 
 // GREENS
-"GREEN_LUM-2" : "#006228",
-"GREEN_LUM-1" : "#007438",
+"GREEN_LUM_-2" : "#006228",
+"GREEN_LUM_-1" : "#007438",
 "GREEN_LUM" : "#20874a", // TARGET
-"GREEN_LUM+1" : "#399b5c",
-"GREEN_LUM+2" : "#4eaf6e",
+"GREEN_LUM_+1" : "#399b5c",
+"GREEN_LUM_+2" : "#4eaf6e",
 
-"GREEN_CHR-2" : "#5e7e66",
-"GREEN_CHR-1" : "#468358",
+"GREEN_CHR_-2" : "#5e7e66",
+"GREEN_CHR_-1" : "#468358",
 "GREEN_CHR" : "#20874a", // TARGET
-"GREEN_CHR+1" : "#008d33",
-"GREEN_CHR+2" : "#00920c",		
+"GREEN_CHR_+1" : "#008d33",
+"GREEN_CHR_+2" : "#00920c",		
 
-"GREEN_HUE-2" : "#6b7e21",
-"GREEN_HUE-1" : "#4d8433",
+"GREEN_HUE_-2" : "#6b7e21",
+"GREEN_HUE_-1" : "#4d8433",
 "GREEN_HUE" : "#20874a", // TARGET
-"GREEN_HUE+1" : "#008a67",
-"GREEN_HUE+2" : "#008b82",
+"GREEN_HUE_+1" : "#008a67",
+"GREEN_HUE_+2" : "#008b82",
 
 //REDS
-"RED_LUM-2" : "#9e210f",
-"RED_LUM-1" : "#b63821",
+"RED_LUM_-2" : "#9e210f",
+"RED_LUM_-1" : "#b63821",
 "RED_LUM" : "#cd4c32", // TARGET
-"RED_LUM+1" : "#e45f43",
-"RED_LUM+2" : "#fe7657",
+"RED_LUM_+1" : "#e45f43",
+"RED_LUM_+2" : "#fe7657",
 
-"RED_CHR-2" : "#aa6453",
-"RED_CHR-1" : "#bc5942",
+"RED_CHR_-2" : "#aa6453",
+"RED_CHR_-1" : "#bc5942",
 "RED_CHR" : "#cd4c32", // TARGET
-"RED_CHR+1" : "#dc391f",
-"RED_CHR+2" : "#eb1607",
+"RED_CHR_+1" : "#dc391f",
+"RED_CHR_+2" : "#eb1607",
 
-"RED_HUE-2" : "#d83862",
-"RED_HUE-1" : "#d54049",
+"RED_HUE_-2" : "#d83862",
+"RED_HUE_-1" : "#d54049",
 "RED_HUE" : "#cd4c32", // TARGET
-"RED_HUE+1" : "#c05819",
-"RED_HUE+2" : "#ae6500",
+"RED_HUE_+1" : "#c05819",
+"RED_HUE_+2" : "#ae6500",
 
 //YELLOWS
-"YELLOW_LUM-2" : "#b19e41",
-"YELLOW_LUM-1" : "#c6b254",
+"YELLOW_LUM_-2" : "#b19e41",
+"YELLOW_LUM_-1" : "#c6b254",
 "YELLOW_LUM" : "#dbc667", // TARGET
-"YELLOW_LUM+1" : "#f1da7a",
-"YELLOW_LUM+2" : "#fff08e",
+"YELLOW_LUM_+1" : "#f1da7a",
+"YELLOW_LUM_+2" : "#fff08e",
 
-"YELLOW_CHR-2" : "#d1c6a3",
-"YELLOW_CHR-1" : "#d7c685",
+"YELLOW_CHR_-2" : "#d1c6a3",
+"YELLOW_CHR_-1" : "#d7c685",
 "YELLOW_CHR" : "#dbc667", // TARGET
-"YELLOW_CHR+1" : "#dec644",
-"YELLOW_CHR+2" : "#dfc602",
+"YELLOW_CHR_+1" : "#dec644",
+"YELLOW_CHR_+2" : "#dfc602",
 
-"YELLOW_HUE-2" : "#ffaf7d",
-"YELLOW_HUE-1" : "#f4bc6a",
+"YELLOW_HUE_-2" : "#ffaf7d",
+"YELLOW_HUE_-1" : "#f4bc6a",
 "YELLOW_HUE" : "#dbc667", // TARGET
-"YELLOW_HUE+1" : "#bfce6d",
-"YELLOW_HUE+2" : "#9fd57d"};
+"YELLOW_HUE_+1" : "#bfce6d",
+"YELLOW_HUE_+2" : "#9fd57d"};
 
 /* Returns array of all subconditions for a given color. 
  * Length 234 in total (78 x 3).
@@ -132,7 +132,8 @@ function get_subcondition_set(color, dimension){
 
 	// Push for each set size
 	for (let size of SET_SIZES){
-		subconditions = subconditions.concat(construct_subcondition(target_path, target_name, opposite_path, opposite_name, size));
+		let hex_code = "#dbc667" // dummy hex_code
+		subconditions = subconditions.concat(construct_subcondition(target_path, target_name, opposite_path, opposite_name, size, hex_code));
 	}
 
 	// Push all other targets
@@ -141,7 +142,8 @@ function get_subcondition_set(color, dimension){
 			for (let distance of DISTANCES){
 				let distractor_path = construct_path(color, dimension, distance);
 				let distractor_name = construct_name(color, dimension, distance);
-				subconditions = subconditions.concat(construct_subcondition(target_path, target_name, distractor_path, distractor_name, size));
+				let hex_code = get_hex_code(color, dimension, distance);
+				subconditions = subconditions.concat(construct_subcondition(target_path, target_name, distractor_path, distractor_name, size, hex_code));
 			}
 		}
 	}	
@@ -163,6 +165,21 @@ function get_opposite_target(color, dimension){
 	return construct_path(opposite_color, dimension, null);
 }
 
+/* Returns the hexadecimal code of a given color.
+ *
+ * @ param color            {string} "BLUE", "GREEN", "RED", "YELLOW"
+ * @ param dimension        {string} "LUM", "CHR", "HUE"
+ * @ param distance         {string} "-2", "-1", "+1", "+2"
+ *
+ * @ return                 {string} code of hex
+ */
+function get_hex_code(color, dimension, distance){
+	
+	let hex_code = construct_name(color, dimension, distance);
+
+	return HEX_COLORS[hex_code];
+}
+
 /* Constructs subcondition assoc array containing target path, distractor path,
  * distractor size and whether target is present/not present.
  *	
@@ -171,12 +188,25 @@ function get_opposite_target(color, dimension){
  * @ param distractor 		{string} Path to distractor
  * @ param distractor_name  {string} Name of distractor color
  * @ param size 			{int}	 Size of set
+ * @ param hex_code         {string} Hex code of distractor color
  *
  * @ return 				{array}  containing 2 assoc arrays, one for target present, one for target false
  */
-function construct_subcondition(target, target_name, distractor, distractor_name, size){
-	return [ {"target_path": target, "target_name": target_name, "distractor_path": distractor, "distractor_name": distractor_name, "size": size, "target_present": true},
-			 {"target_path": target, "target_name": target_name, "distractor_path": distractor, "distractor_name": distractor_name, "size": size, "target_present": false}];
+function construct_subcondition(target, target_name, distractor, distractor_name, size, hex_code){
+	return [ {"target_path": target, 
+			  "target_name": target_name, 
+			  "distractor_path": distractor, 
+			  "distractor_name": distractor_name, 
+			  "size": size, 
+			  "target_present": true,
+			  "hex_code": hex_code},
+			 {"target_path": target, 
+			  "target_name": target_name, 
+			  "distractor_path": distractor, 
+			  "distractor_name": distractor_name, 
+			  "size": size, 
+			  "target_present": false,
+			  "hex_code": hex_code}];
 }
 
 /* Constructs path to the svg.
